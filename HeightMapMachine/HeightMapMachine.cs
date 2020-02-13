ccfa389317953659b08ad9eec38a1e6c4e8ddb44
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2020-02-13 04:15:48 +0300

    Start height map machine

---
+++ HeightMapMachine/HeightMapMachine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Machine;
+
+namespace HeightMapMachine
+{
+    public class HeightMapMachine : IMachine
+    {
+        public State RunState { get; private set;  }
+
+        public void Reboot()
+        {
+
+        }
+
+        public void Abort()
+        {
+
+        }
+
+        public void Activate()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void Continue()
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
